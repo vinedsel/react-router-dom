55dcb2c01128d6979a5d69e9dba0a6ec817e4722
Raymond Gregory <vinedsel@gmail.com>
2019-01-05 23:18:46 -0600

    Modified index.js.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './screens/Home';
+import About from './screens/About';
+import Topics from './screens/Topics';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));

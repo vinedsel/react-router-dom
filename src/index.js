88d860ee1fef04d5de3b849f38d520ae8a9cbdd3
Raymond Gregory <vinedsel@gmail.com>
2019-01-05 23:32:33 -0600

    Added nav code.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 const App = () => (
   <Router>
     <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/topics">Topics</Link>
+      </li>
+    </ul>
+
+    <hr />
+
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/topics" component={Topics} />
